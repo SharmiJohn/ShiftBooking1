c15ff01ce7e6eb046c6941e6b0399727dd804fe6
SharmiJohn <1422000joselvi@gmail.com>
2024-10-11 11:31:44 +0530

    fixed1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 
 import './App.css';
-
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import MyShift from './Pages/MyShift';
 import AvailableShift from './Pages/AvailableShift';
 import { useState } from 'react';
-function App() {
-  const [activeLink,setactiveLink]=useState("/MyShift");
-  const handleClick=(link)=>{
-    setactiveLink(link);
 
+function App() {
+  const [activeLink, setActiveLink] = useState("/MyShift");
+
+  const handleClick = (link) => {
+    setActiveLink(link);
   }
+
   return (
     <BrowserRouter>
-     <nav className='App'>
-      <Link to="/MyShift" onClick={()=>handleClick('/MyShift')} className={activeLink==='/MyShift'?"active":" "}>My Shift </Link>
-      
-      <Link to="/AvailableShift" onClick={()=>handleClick('/AvailableShift')}  className={activeLink==='/AvailableShift'?"active":" "}> Available Shift</Link>
-     </nav>
-     <Routes>
-      <Route path="/MyShift" element={<MyShift/>}/>
-      <Route path="/AvailableShift" element={<AvailableShift/>}/>
-     </Routes>
+      <nav className='App'>
+        <Link to="/MyShift" onClick={() => handleClick('/MyShift')} className={activeLink === '/MyShift' ? "active" : " "}>
+          My Shift
+        </Link>
+        
+        <Link to="/AvailableShift" onClick={() => handleClick('/AvailableShift')} className={activeLink === '/AvailableShift' ? "active" : " "}>
+          Available Shift
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Navigate to="/MyShift" replace />} /> {/* Redirect to /MyShift */}
+        <Route path="/MyShift" element={<MyShift />} />
+        <Route path="/AvailableShift" element={<AvailableShift />} />
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
